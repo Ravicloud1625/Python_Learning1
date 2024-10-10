0931551e5240534824e3578d4aedc69f2d87b22d
Ravicloud1625 <107475734+Ravicloud1625@users.noreply.github.com>
2024-10-10 14:03:40 +0400

    Create f1.py
    f1 function to provide input

---
+++ f1.py
@@ -0,0 +1,13 @@
+import f2,random
+
+a=int(random.randrange(11,15))
+b=int(random.randrange(6,10))
+c=int(random.randrange(1,5))
+
+add1,sub1,mul1 = f2.calc(a,b,c)
+high=f2.compare(a,b,c)
+f2.table(c)
+
+
+
+
